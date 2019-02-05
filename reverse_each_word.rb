245ecd346c1074605fe7bce8fd4ca23345fe1a65
Nicholas Dover <dovern42@gmail.com>
2019-02-05 06:14:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
-def reverse_each_word(sentence)
-  array = sentence.split(" ")
-  array2 = []
-  array.each do |word|
-    array2.push(word.reverse)
-  end
-  array2.join(" ")
-end
+#def reverse_each_word(sentence)
+#  array = sentence.split(" ")
+#  array2 = []
+#  array.each do |word|
+#    array2.push(word.reverse)
+#  end
+#  array2.join(" ")
+#end
 
 def reverse(array)
   array.collect do |word|
